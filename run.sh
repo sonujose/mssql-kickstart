f87d8e46478bcb6bb4265c1f5d6d21bd1789c587
Sonu Jose <sonujose993@gmail.com>
2020-03-25 20:56:05 +0530

    updates

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 # Start a mssql-server instance running as the SQL Express edition
-docker run -e 'ACCEPT_EULA=Y' 
-           -e 'SA_PASSWORD=yourStrong(!)Password' 
-           -e 'MSSQL_PID=Express' 
-           -p 1433:1433 
-           -d mcr.microsoft.com/mssql/server:2017-latest-ubuntu 
+docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=yourStrong(!)Password' -e 'MSSQL_PID=Express' -p 1433:1433 -d mcr.microsoft.com/mssql/server:2017-latest-ubuntu
